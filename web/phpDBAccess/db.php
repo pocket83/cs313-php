a4493403a9a7ed6277c16e17541cb3ac88acbb19
Wes Clark <Wes Clark>
2016-10-17 06:27:17 -0600

    testing the example db

--- web/phpDBAccess/db.php
+++ web/phpDBAccess/db.php
@@ -1,38 +1,42 @@
 <html>
-
 <body>
-    <h1>Users</h1>
-    <?php
+
+<?php
 
 // default Heroku Postgres configuration URL
+$dbUrl = getenv('DATABASE_URL');
 
-$dbUrl = getenv('postgres://hdghgzswtmkufg:zG8qDA8ZElYXvqeGbhvYJwhMMs@ec2-54-235-155-172.compute-1.amazonaws.com:5432/d5pf61j61flag6');
+if (empty($dbUrl)) {
+ // example localhost configuration URL with postgres username and a database called cs313db
+ $dbUrl = "postgres://postgres:password@localhost:5432/cs313db";
+}
 
 $dbopts = parse_url($dbUrl);
 
+print "<p>$dbUrl</p>\n\n";
 
-$dbHost = $dbopts["host"];
-$dbPort = $dbopts["port"];
-$dbUser = $dbopts["user"];
+$dbHost = $dbopts["host"]; 
+$dbPort = $dbopts["port"]; 
+$dbUser = $dbopts["user"]; 
 $dbPassword = $dbopts["pass"];
 $dbName = ltrim($dbopts["path"],'/');
 
+print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
+
 try {
-$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-    foreach($db->query('SELECT first_name, last_name, email FROM users')as $row)
-    {
-        echo '<p>';
-        echo '<strong>' . $row['first_name'] . '' . $row['last_name'] . '';
-        echo $row['email'];
-        echo '</p>';
-    }
+ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 }
 catch (PDOException $ex) {
-print "<p>error: $ex->getMessage() </p>\n\n";
-die();
+ print "<p>error: $ex->getMessage() </p>\n\n";
+ die();
+}
+
+foreach ($db->query('SELECT now()') as $row)
+{
+ print "<p>$row[0]</p>\n\n";
 }
 
 ?>
-</body>
 
+</body>
 </html>
