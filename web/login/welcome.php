ac371eb2afb681096e4b620a4cb94e8876f08e43
Wes Clark <Wes Clark>
2016-10-31 04:26:30 -0600

    ..

--- web/login/welcome.php
+++ web/login/welcome.php
@@ -52,7 +52,7 @@
                 foreach ($db->query($q) as $row) {
                     
                     if ($password == $row['password']) {
-                        $uers = $row['irst_name'];
+                        $uers = $row['first_name'];
                         echo "name is set! ";
                         echo "name is: ";
                         echo $user;
