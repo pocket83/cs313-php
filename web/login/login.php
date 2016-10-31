2ab71bddf62ca3aefad8ded4d66a9757caacf089
Wes Clark <Wes Clark>
2016-10-30 19:33:35 -0600

    changing some things...

--- web/login/login.php
+++ web/login/login.php
@@ -35,32 +35,7 @@
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="js/materialize.min.js"></script>
     
-    <!-- DB ACCESS -->
-    <?php
-        // default Heroku Postgres configuration URL
-        $dbUrl = getenv('DATABASE_URL');
-
-        if (empty($dbUrl)) {
-         // example localhost configuration URL with postgres username and a database called cs313db
-         $dbUrl = "postgres://hdghgzswtmkufg:zG8qDA8ZElYXvqeGbhvYJwhMMs@ec2-54-235-155-172.compute-1.amazonaws.com:5432/d5pf61j61flag6";
-        }
-
-        $dbopts = parse_url($dbUrl);
-
-        $dbHost = $dbopts["host"]; 
-        $dbPort = $dbopts["port"]; 
-        $dbUser = $dbopts["user"]; 
-        $dbPassword = $dbopts["pass"];
-        $dbName = ltrim($dbopts["path"],'/');
-
-        try {
-         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-        }
-        catch (PDOException $ex) {
-         print "<p>error: $ex->getMessage() </p>\n\n";
-         die();
-        }
-    ?>
+    
     
     
     <!-- NAVIGATION -->
--- web/login/register.php
+++ web/login/register.php
@@ -47,6 +47,7 @@
         if ($_SERVER['REQUEST_METHOD'] == "POST")
         {
             $first_name = $_POST["first_name"];
+            echo $_POST["first_name"];
             $last_name = $_POST["last_name"];
             $email = $_POST["email"];
             $password = $_POST["password"];
